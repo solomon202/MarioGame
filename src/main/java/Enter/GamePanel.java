c809de911f2f3a5441f5014f11426e404e332bd9
vasy <https://github.com/solomon202/NewsApp>
2023-02-09 22:21:00 +0300

    three

--- src/main/java/Enter/GamePanel.java
+++ src/main/java/Enter/GamePanel.java
@@ -1,19 +1,27 @@
 package Enter;
 
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
+import inputs.Keyboardsinput;
+
 public class GamePanel extends JPanel{
+	
 	
    public GamePanel() {
 	   
+	   addKeyListener(new Keyboardsinput());
+	   
    }
+	   
    
    public void paintComponent(Graphics g) {
 	   super.paintComponent(g);
 	   
-	   g.drawRect(100, 100,200 , 50);
+	   g.fillRect(100, 100,200 , 50);
 	   
    
    }
