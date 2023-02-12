8baf6f2d98a0db51b337d3c318b896b91b8818de
vasy <https://github.com/solomon202/NewsApp>
2023-02-12 20:03:03 +0300

    four

--- src/main/java/Enter/GamePanel.java
+++ src/main/java/Enter/GamePanel.java
@@ -7,21 +7,40 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 
 import inputs.Keyboardsinput;
+import inputs.MouseInputs;
 
 public class GamePanel extends JPanel{
+	//есть уже заданые параметры или получить через конструктор изменяемые данные .
+	private MouseInputs mouseInputs;
+	private int xDelta = 0,yDelta = 0;
 	
-	
-   public GamePanel() {
+   public GamePanel() { 
 	   
-	   addKeyListener(new Keyboardsinput());
+	   mouseInputs = new MouseInputs();                                        
+	   // для обработки событий клавиатуры необходимо реализовать специальный интерфейс, а затем добавить получившегося слушателя к интересуемому компоненту. 
+	   addKeyListener(new Keyboardsinput(this));
+	   //эти интерфейса содержат методы, принимающие и обрабатывающие различные типы событий от мыши
+	   addMouseListener(mouseInputs);
+	   addMouseMotionListener(mouseInputs);
 	   
    }
-	   
+   //метод с получаемым параметром 
+   public void changeXDelta(int value) {
+	   //есть какаята картинка изночально позицыя квадрата 100 потом при нажатии присваиваиваится новая позиция дельта 0 и перересовыется уже не 0
+	   //тоесть  это позиция и + новая дэльта.
+	   this.xDelta += value;
+	   repaint();
+   }
+   
+   public void changeYDelta(int value) {
+	   this.yDelta += value;
+	   repaint();
+   }
    
    public void paintComponent(Graphics g) {
 	   super.paintComponent(g);
-	   
-	   g.fillRect(100, 100,200 , 50);
+	   //прямоугольник 100+дельта его позицыя
+	   g.fillRect(100+xDelta, 100+yDelta,200 , 50);
 	   
    
    }
--- src/main/java/inputs/Keyboardsinput.java
+++ src/main/java/inputs/Keyboardsinput.java
@@ -3,12 +3,37 @@ package inputs;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import Enter.GamePanel;
+
 public class Keyboardsinput  implements KeyListener {
+	//есть доступ к игровой панели в нутри клавиотуры
+	//сылка 
+	private GamePanel gamePanel;
+	//получения реальной сылки 
+	public Keyboardsinput(GamePanel gamePanel) {
+		//присваивание реального обьекта 
+		this.gamePanel = gamePanel;
+	}
 	
 		@Override
 		public void keyPressed(KeyEvent e) {
 			
-	System.out.println("opppp");
+	  switch (e.getKeyCode()) {
+	  case KeyEvent.VK_W:
+		  //ПОЛУЧИЛИ доступ к игровой панели  СЫЛКИ НА ИГРАВУЮ ПАНЕЛЬ И ВСТАВИЛИ параметры
+		  gamePanel.changeYDelta(-5);
+		  break;
+	  case KeyEvent.VK_A:
+		  gamePanel.changeXDelta(-5);
+		  break;
+	  case KeyEvent.VK_S:
+		  gamePanel.changeYDelta(5);
+		  break;
+	  case KeyEvent.VK_D:
+		  gamePanel.changeXDelta(5);
+		  break;
+		  
+	  }
 			
 		}
 
